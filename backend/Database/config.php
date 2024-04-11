09b8a7ecdcf268bcc6b1856e7a9c5ab0ef53bba0
faustinopsy <rodrigohipnose@gmail.com>
2024-04-11 00:03:54 -0300

    new file:   backend/Database/banco_x.db 	modified:   backend/Database/config.php

--- backend/Database/config.php
+++ backend/Database/config.php
@@ -8,8 +8,8 @@
   define('DB_HOST', 'localhost');
   define('DB_NAME', 'loja');
   define('DB_USER', 'root');
-  define('DB_PASSWORD', 'root123');
-  define('DB_TYPE', 'mysql');
+  define('DB_PASSWORD', '');
+  define('DB_TYPE', 'sqlite');
   define('TOKEN', '9b426114868f4e2179612445148c4985429e5138758ffeed5eeac1d1976e7443');
     
   
